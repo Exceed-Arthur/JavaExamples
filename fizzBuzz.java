10fe3cc9f8cd2b27901bd182319cf71c2afb54a2
Exceed IO Inc <arthurlee087@gmail.com>
2022-05-05 14:24:11 -0500

    Create fizzBuzz.java
    Basic FizzBuzz Example

---
+++ fizzBuzz.java
@@ -0,0 +1,26 @@
+public class fizzBuzz {
+   public static void main(String args[]) {
+      int i;
+      for (i=1; i<100; i++) {
+            String response;
+            response = fizz(i);
+            System.out.println(response);
+      }
+   }
+      public static String fizz(int current_fizzeration) 
+   {
+      String iterative_results;
+      if (current_fizzeration % 15 == 0) {
+          return "FizzBuzz";
+   }
+      else if (current_fizzeration % 5 == 0) {
+         return "Buzz";
+   }
+      else if (current_fizzeration % 3 == 0) {
+         return "Fizz";
+   }
+      else {
+         return String.valueOf(current_fizzeration);
+      }
+   }
+}
