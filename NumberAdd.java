2ef2b6a2326cf4144365d03135b01044b3804a28
Exceed IO Inc <arthurlee087@gmail.com>
2022-05-09 14:56:36 -0500

    Create NumberAdd.java

---
+++ NumberAdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;  // Import the Scanner class
+
+class NumberAdd {
+    
+    public static void main(String[] args) {
+        
+        int summed = added(256, 141);
+    }
+        
+    public static int added(int x, int y){
+        System.out.println(x+y);
+        int z = x+y;
+        return z;
+    }
+}
