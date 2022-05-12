bc372942d8dc409155aa8a2a79cced6cba156bdc
Exceed IO Inc <arthurlee087@gmail.com>
2022-05-12 17:31:30 -0500

    Create JavaAsci.java

---
+++ JavaAsci.java
@@ -0,0 +1,8 @@
+class JavaAsci { // This class will print out a Pattern on Console that spells and displays as Java
+    public static void main(String[] args) {
+        System.out.println("   J    a   v     v  a   \n"); // The Escape Character and New Line Character cerates new line
+        System.out.println("   J   a a   v   v  a a  \n");
+        System.out.println("J  J  aaaaa   V V  aaaaa \n");
+        System.out.println(" JJ  a     a   V  a     a\n");
+    }
+}
