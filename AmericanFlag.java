0be8d56ed9661ca3b06744f78eed11deb332a1ee
Exceed IO Inc <arthurlee087@gmail.com>
2022-05-12 17:52:36 -0500

    Create AmericanFlag.java
    Print an American Flag on Console

---
+++ AmericanFlag.java
@@ -0,0 +1,37 @@
+class AmericanFlag {
+    public static void main(String[] args) {
+        System.out.println(Flag());
+    }
+    private static String Flag() {
+        String newLine = "\n";
+        return "* * * * * * =================================="
+                .concat(newLine)
+                .concat(" * * * * *  ==================================")
+                .concat(newLine)
+                .concat("* * * * * * ==================================")
+                .concat(newLine)
+                .concat(" * * * * *  ==================================")
+                .concat(newLine)
+                .concat("* * * * * * ==================================")
+                .concat(newLine)
+                .concat(" * * * * *  ==================================")
+                .concat(newLine)
+                .concat("* * * * * * ==================================")
+                .concat(newLine)
+                .concat(" * * * * *  ==================================")
+                .concat(newLine)
+                .concat("* * * * * * ==================================")
+                .concat(newLine)
+                .concat("==============================================")
+                .concat(newLine)
+                .concat("==============================================")
+                .concat(newLine)
+                .concat("==============================================")
+                .concat(newLine)
+                .concat("==============================================")
+                .concat(newLine)
+                .concat("==============================================")
+                .concat(newLine)
+                .concat("==============================================");
+    }
+}
