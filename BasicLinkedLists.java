337e36849877ed047065de743ceb10f54f2a8a7b
Exceed IO Inc <arthurlee087@gmail.com>
2022-05-18 01:59:03 -0500

    Update BasicLinkedLists.java

--- BasicLinkedLists.java
+++ BasicLinkedLists.java
@@ -4,28 +4,39 @@ import java.util.Collections;
 import java.util.function.Supplier;
 import java.lang.Math.*;
 
-
-
 class LinkedLists {
-
     public static void main(String[] args) {
-        Node head = new Node(RandomBig());
+        int pre_int1 = (int) Math.round(Math.floor(Math.random() * 99999.99));
+        Double random1 = RandomBig();
+        Node head = new Node(random1, "Head");
         List<Node> LinkedListPre = new ArrayList<Node>(); 
         LinkedListPre.add(head);
-        Double Big = RandomBig();
-        int i;
+        int i = 0;
         Node current = head;
-        for (i=0; i < Big; i++) {
-            Node Node2 = new Node(RandomBig());
+        System.out.println(String.format("\n%d. Initializing LinkedList Tutorial Demo...\n_____________________________________________\n\nCreated Root Node with Data: %.2f", (i+1), random1));
+        for (i=0; i < random1; i++) {
+            int pre_int = (int) Math.round(Math.floor(Math.random() * 99999.99));
+            String Name = String.valueOf(pre_int);
+            Double random = RandomBig();
+            Node Node2 = new Node(random, Name);
             current.next = Node2;
             current = Node2;
             LinkedListPre.add(Node2);
+            System.out.println(String.format("\n%d. Created Node %s -> Current Data: %.2f", (i+1), Node2, random));
         }
-  
-
         int length_of_linked_list = LinkedListPre.size();
         Double head_data = head.data;
-        System.out.println(String.format("Successfully created linked list with %d nodes.\nThe root contains the data -> int data:  %d", length_of_linked_list, head_data));
+        System.out.println(String.format("Successfully created linked list with %d nodes.\nThe root contains the data -> int data:  %.2f", length_of_linked_list, head_data));
+        int ij;
+        System.out.println("\nInitializing Graph Traversal!\n_______________________________________________\n");
+        for (ij=0; ij<length_of_linked_list; ij++) {
+            Node node = LinkedListPre.get(ij);
+            try {
+            System.out.println(String.format("%d. Current Node: (%s=%.2f  -<- Next Node -> (%s=%.2f)", (ij+1), node, node.data, node.next, node.next.data));
+            } catch (Exception e) {
+                System.out.println(String.format("%d. Current Node: (%s=%.2f)  -<- Next Node -> %s", (ij+1), node, node.data, null));
+            }
+        }
     }
 
     public static int CountNodes(Node root) {
@@ -39,7 +50,6 @@ class LinkedLists {
         return count;
     }
 
-    
     public static List GenerateRandomLinkedList() {
         List<Node> Nodes = new ArrayList<Node>();
         int iterations = (int) Math.round(RandomBig()) + 1;
@@ -49,29 +59,40 @@ class LinkedLists {
         }
         return Nodes;
     }
+    
     public static Double RandomBig() {
         Double random_1 = Math.random();
         Double oneth = Math.floor(random_1 * 100.00 + 1.00 - random_1);
         return oneth;
     }
+    
     private static Node CreateNodeRandom() {
-        Node node = new Node(RandomBig());
+        int pre_int = (int) Math.round(Math.floor(Math.random() * 99999.99));
+        String Name = String.valueOf(pre_int);
+        Node node = new Node(RandomBig(), Name);
         return node;
     }
     
     public static class Node {
         Double data;
-        public Node(Double data) {
-            data = data;
+        String name;
+        public Node(Double data, String name) {
+            this.data = data;
+            this.name = name;
         }
         Node next;
         public void setData(Double data) {
-            data = data;
+            this.data = data;
         }
         public Double getData() {
             return data;
         }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public Double getName() {
+            return data;
+        }
     }
-    
 }
 
