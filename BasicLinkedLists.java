c4162ad304f02102c35517f7c37de083a93044fe
Exceed IO Inc <arthurlee087@gmail.com>
2022-05-18 00:23:16 -0500

    Update BasicLinkedLists.java

--- BasicLinkedLists.java
+++ BasicLinkedLists.java
@@ -1,68 +1,78 @@
+package com.exceed.io.JavaEx;
+
+
 import java.util.*;
 import java.util.Scanner;
 import java.util.Collections;
 import java.util.function.Supplier;
-import java.util.ArrayList.size;
+import java.lang.Math.*;
+
+
+
 class LinkedLists {
+
     public static void Main(String[] args) {
-        RandomLinkedList LinkedListInstance = new RandomLinkedList();
-        List<Node> LinkedList = LinkedListInstance.Nodes;
-        Node head = LinkedList.head;
-        int length_of_linked_list = LinkedList.Size();
+        Node head = new Node(RandomBig());
+        List<Node> LinkedListPre = new ArrayList<Node>(); 
+        LinkedListPre.add(head);
+        int Big = RandomBig();
+        int i;
+        Node current = head;
+        for (i=0; i < Big; i++) {
+            Node Node2 = new Node(RandomBig());
+            current.next = Node2;
+            current = Node2;
+            LinkedListPre.add(Node2);
+        }
+  
+
+        int length_of_linked_list = LinkedListPre.size();
         int head_data = head.data;
         System.out.println(String.format("Successfully created linked list with %d nodes.\nThe root contains the data -> int data:  %d", length_of_linked_list, head_data));
     }
-    public class Node {
-        public Node(int data) {
-            this.data = data;
-        }
 
-        Node next;
-        public static void setData(int data) {
-            this.data = data;
-        }
-        public static int getData() {
-            return this.data;
-        }
-    }
     public static int CountNodes(Node root) {
         Node current = root;
         Node next;
         int count = 0;
-        while current.next != null {
+        while (current.next != null) {
             current = current.next;
             count++;
         }
         return count;
     }
-    public class RandomLinkedList {
-        public RandomLinkedList() {
-        } // Empty Constructor Function
-        Node head = new Node();
-        this.head = head;
-        list<Node> Nodes = new ArrayList<Node>();
-        Nodes.add(head);
-        List LinkedList2 = GenerateRandomLinkedList; 
-        Collections.addAll(Nodes, LinkedList2.toArray());
-        this.Nodes = Nodes;
-    }
-        
+
+    
     public static List GenerateRandomLinkedList() {
         List<Node> Nodes = new ArrayList<Node>();
         int iterations = RandomBig() + 1;
         int i;
-        for (i=0, i < iterations, i++) {
+        for (i=0; i < iterations; i++) {
             Nodes.add(CreateNodeRandom());
         }
         return Nodes;
     }
-    public static Int RandomBig() {
-        random_1 = Math.Random();
-        Int oneth = Math.floor(random_1 * 100 + 1 - random_1);
+    public static int RandomBig() {
+        Double random_1 = Math.random();
+        int oneth = Math.floor(random_1 * 100 + 1 - random_1);
         return oneth;
     }
     public static Node CreateNodeRandom() {
         return Node(RandomBig());
     }
     
+}
+
+class Node {
+    int data;
+    public Node(int data) {
+        data = data;
+    }
+    Node next;
+    public void setData(int data) {
+        data = data;
+    }
+    public int getData() {
+        return data;
+    }
 }
